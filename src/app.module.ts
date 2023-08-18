223eb00081f086026a61d26c597bdade26a63605
Lautaro Decima <decimalautaro@gmail.com>
2023-08-17 21:22:02 -0300

    feat: adding controllers in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
+import { UsersController } from './controllers/users.controller';
+import { OrdersController } from './controllers/orders.controller';
+import { CustomersController } from './controllers/customers.controller';
+import { BrandsController } from './controllers/brands.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    UsersController,
+    OrdersController,
+    CustomersController,
+    BrandsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
