61d13587d546a3060efbadf9b2d781fe56f3161c
Lautaro Decima <decimalautaro@gmail.com>
2023-08-20 03:36:51 -0300

    feat:avoiding incorrect parameters

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,34 @@
+import {
+  IsNumber,
+  IsString,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly stock: number;
+
+  @IsNotEmpty()
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
