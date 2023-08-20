1ec8f38de82dce4a4fdbe2ec4abbf8f5159838b0
Lautaro Decima <decimalautaro@gmail.com>
2023-08-20 18:01:29 -0300

    feat:using useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = 'develop';
+const API_KEY_PROD = 'production';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
