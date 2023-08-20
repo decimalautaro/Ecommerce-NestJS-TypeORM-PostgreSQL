784f3bc643084557132776dcf6c260cd77566371
Lautaro Decima <decimalautaro@gmail.com>
2023-08-19 21:11:14 -0300

    feat: creating product services

---
+++ src/services/products.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from '../entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'product 1',
+      description: 'description 1',
+      price: 122,
+      stock: 23,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
+  }
+
+  create(payload: any) {
+    this.counterId++;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  remove(id: number) {
+    const index = this.products.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    this.products.splice(index, 1); // Cambio a "splice"
+    return true;
+  }
+
+  update(id: number, payload: any) {
+    const product = this.findOne(id);
+    if (product) {
+      const index = this.products.findIndex((item) => item.id === id);
+      this.products[index] = {
+        ...product,
+        ...payload,
+      };
+      return this.products[index];
+    }
+    return {
+      message: `el id: ${id} no es valido`,
+    };
+  }
+}
