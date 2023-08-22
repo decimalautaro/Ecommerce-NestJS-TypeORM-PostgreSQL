76c26091e2d2814f8fcb9f43ad670a6f7a852a5e
Lautaro Decima <decimalautaro@gmail.com>
2023-08-21 22:21:10 -0300

    feat: Schema validation in .envs with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { HttpModule, HttpService } from '@nestjs/axios';
-
-import { lastValueFrom } from 'rxjs';
-
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import * as Joi from 'joi';
 
 import { enviroments } from './enviroments';
 import config from './config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
@@ -25,6 +23,11 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
